9578990a5aa24b840e279f03e8daf8969ef761f8
Emre Yoleri <emreyoleri01@gmail.com>
2022-06-26 21:46:04 +0300

    Using the APIview

--- blog_api/views.py
+++ blog_api/views.py
@@ -2,10 +2,12 @@ from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
 
+
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveDestroyAPIView):
-    pass
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
