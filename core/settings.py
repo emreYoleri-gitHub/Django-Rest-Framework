69958dff882c5093f2eedc31a1fbc3a94f5f4a27
Emre Yoleri <emreyoleri01@gmail.com>
2022-06-27 13:07:42 +0300

    Project level permissions

--- core/settings.py
+++ core/settings.py
@@ -129,9 +129,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.IsAuthenticatedOrReadonly'
     ]
 }
+
+# ! Permissions:
+# AllowAny
+# IsAuthenticated
+# IsAdminUser
+# IsAuthenticatedOrReadonly
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
