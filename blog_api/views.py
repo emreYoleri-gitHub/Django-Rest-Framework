404838a55facbec0ed71d073ab68c427d3ea4732
Emre Yoleri <emreyoleri01@gmail.com>
2022-06-27 13:44:13 +0300

    Creating a custom permission

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,7 +1,16 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import IsAdminUser, DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.permissions import BasePermission, IsAdminUser, DjangoModelPermissionsOrAnonReadOnly, SAFE_METHODS
+
+
+class PostUserWritePermission(BasePermission):
+    message = "Editing posts is restricted to the author only."
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
 
 
 class PostList(generics.ListCreateAPIView):
@@ -10,6 +19,7 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
