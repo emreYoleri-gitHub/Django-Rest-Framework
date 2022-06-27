db57d71d102f750150ab55dc2facaa12578dab18
Emre Yoleri <emreyoleri01@gmail.com>
2022-06-27 13:20:11 +0300

    Activating user login URLs for the APIview

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, DjangoModelPermissionsOrAnonReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = [IsAdminUser]
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
