1e2a73f67f5fcbb97befa6f5235f71230423bc79
Emre Yoleri <emreyoleri01@gmail.com>
2022-06-27 13:17:03 +0300

    View level permissions

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAdminUser
 
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [IsAdminUser]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
